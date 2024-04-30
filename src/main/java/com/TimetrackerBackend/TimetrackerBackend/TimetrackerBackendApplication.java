edbb0f0f9161272721856e85ea9634a43d5295e5
Mathias Brynolf <mathiasbrynolf20@gmail.com>
2024-04-30 12:40:27 +0200

    .

---
+++ src/main/java/com/TimetrackerBackend/TimetrackerBackend/TimetrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.TimetrackerBackend.TimetrackerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TimetrackerBackend/TimetrackerBackend/TimetrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimetrackerBackend.TimetrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
